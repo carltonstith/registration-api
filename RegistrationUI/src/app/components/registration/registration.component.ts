4f82fd89daf6ac4c0348d860ba79ed263bbb5619
Carlton Stith <carlton215@gmail.com>
2020-06-18 22:54:02 -0400

    app structure doc, component and services folders created

---
+++ RegistrationUI/src/app/components/registration/registration.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  template: `
+    <p>
+      registration works!
+    </p>
+  `,
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
